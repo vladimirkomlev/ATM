6b95163935c0efffcf6eea62461c8c12f6a3234a
vladimirkomlev <vladimir.komlev00@gmail.com>
2016-10-10 01:55:12 +0300

    removed all comments from PageOperationAcceptInputCash

--- src/main/java/ua/dataart/school/atm/servlet/PageOperationAcceptInputCash.java
+++ src/main/java/ua/dataart/school/atm/servlet/PageOperationAcceptInputCash.java
@@ -15,8 +15,6 @@ import ua.dataart.school.atm.operations.behavior.RequiredAmount;
 @WebServlet("/putcash")
 public class PageOperationAcceptInputCash extends PageSelectOperation {
 
-	// TODO: 8/11/16 eugene - you don't need serialVersionUID
-	// vova - IDE require create serialVersionUID
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOG = Logger.getLogger(PageOperationAcceptInputCash.class);
 	private static final String JSP_PUTCASH_PATH = "WEB-INF/jsp/putcash.jsp";
@@ -29,11 +27,9 @@ public class PageOperationAcceptInputCash extends PageSelectOperation {
 		request.getRequestDispatcher(JSP_PUTCASH_PATH).forward(request, response);
 	}
 
-	// TODO: 8/11/16 eugene - overcomplicated method, rework
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String message;
 		String informationTransaction = "Receipt in cash-";
 		boolean resultChoose = true;
 		boolean resultOfValidaionInputValues = validationOfInputValuesImpl.validationOfInputValues(request);
@@ -56,38 +52,5 @@ public class PageOperationAcceptInputCash extends PageSelectOperation {
 		}else{
 			sendValuesToJsp(inputRequiredAmount.getOutputMessage(), resultChoose, JSP_PUTCASH_PATH, request, response);
 		}
-		
-//		int inputSum = operationOfBanknoteImpl.getAmountFromInputStorageOfBanknotes(banknoteStorage.getBanknotes());
-//		int resultSum = 0;
-//		try {
-//			resultSum = operationOfBanknoteImpl.acceptInputCash(banknoteStorage);
-//		} catch (CloneNotSupportedException e) {
-//			LOG.info("The cloning operation failed. " + e.fillInStackTrace());
-//		}
-//		if (inputSum == 0) {
-//			message = "Entered amount should be greater than zero. Please, enter the correct value.";
-//			sendValuesToJsp(message, resultChoose, JSP_PUTCASH_PATH, request, response);
-//		} else if (resultSum == 0) {
-//			message = "Unfortunately we can not accept your input amount. The number of banknotes filled in that you specified.";
-//			sendValuesToJsp(message, resultChoose, JSP_PUTCASH_PATH, request, response);
-//		} else if (inputSum == resultSum) {
-//			try {
-//				operationOfBanknoteImpl.saveCurrentStorageInMemory();
-//				saveInformationInLog(informationTransaction);
-//			} catch (CloneNotSupportedException e) {
-//				LOG.info("The cloning operation failed. " + e.fillInStackTrace());
-//			}
-//			message = "Credited with the amount " + resultSum;
-//			sendValuesToJsp(message, resultChoose, JSP_PUTCASH_PATH, request, response);
-//		} else if (inputSum > resultSum) {
-//			message = "Unfortunately we can not accept you have the amount " + inputSum
-//					+ " of these denominations of banknotes. We can take from you amount " + resultSum
-//					+ " from the following denominations of banknotes:";
-//			banknoteStorage.setBanknotes(operationOfBanknoteImpl.getSaveStorage());
-//			servletContext.setAttribute("resultSum", resultSum);
-//			servletContext.setAttribute("selectedOperation", 0);
-//			servletContext.setAttribute("informationTransaction", informationTransaction);
-//			sendValuesToJsp(message, resultChoose, JSP_ANOTHERSUM_PATH, request, response);
-//		}
 	}
 }
